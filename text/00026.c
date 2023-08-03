1e6a85cc588b7376d3f663d87cfeb268427ed474
Karine EM <karineven79@gmail.com>
2023-08-04 00:30:10 +0100

    Create 00026.c

---
+++ text/00026.c
@@ -0,0 +1,9 @@
+// Modification timestamp: 2023-08-03 17:15:30
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00026.c
+
+int main() {
+    char *p;
+
+    p = "hello";
+    return p[0] - 104;
+}
