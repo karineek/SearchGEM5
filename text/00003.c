29187b7b241e342c5e85d5935b40003885234a11
Karine EM <karineven79@gmail.com>
2023-08-03 19:49:32 +0100

    Update 00003.c

--- text/00003.c
+++ text/00003.c
@@ -1,10 +1,9 @@
-User
-// Taken from https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00003.c
-int
-main()
-{
-	int x;
-	
-	x = 4;
-	return x - 4;
+// Modification timestamp: 2023-08-02 17:40:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00003.c
+
+int main() {
+  int x;
+
+  x = 4;
+  return x - 4;
 }
