8fc69595db4bf40bd2ab28be8b917880360255e7
Karine EM <karineven79@gmail.com>
2023-08-03 21:54:00 +0100

    Create 00012.c

---
+++ 00012.c
@@ -0,0 +1,6 @@
+// Modification timestamp: 2023-08-02 17:40:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00012.c
+
+int main() {
+    return (2 + 2) * 2 - 8;
+}
