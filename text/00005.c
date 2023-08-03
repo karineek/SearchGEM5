4ac7f20aa7f208a63612fff459e756f7ec969732
Karine EM <karineven79@gmail.com>
2023-08-03 19:44:21 +0100

    Create 00005.c

---
+++ text/00005.c
@@ -0,0 +1,26 @@
+// Modification timestamp: 2023-08-02 17:15:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00005.c
+
+#include <stdio.h>
+
+int main() {
+  int x;
+  int *p;
+  int **pp;
+
+  x = 0;
+  p = &x;
+  pp = &p;
+
+  if (*p)
+    return 1;
+  if (**pp)
+    return 1;
+  else
+    **pp = 1;
+
+  if (x)
+    return 0;
+  else
+    return 1;
+}
