b4bb463307d4c2bc0184db0baeef1241e567e36c
Karine EM <karineven79@gmail.com>
2023-08-03 21:51:18 +0100

    Create 00011.c

---
+++ text/00011.c
@@ -0,0 +1,9 @@
+// Modification timestamp: 2023-08-02 17:40:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00011.c
+
+int main() {
+    int x;
+    int y;
+    x = y = 0;
+    return x;
+}
