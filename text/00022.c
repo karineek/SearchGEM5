45226cb0d56397debc3e982b0f656d9de0500a32
Karine EM <karineven79@gmail.com>
2023-08-03 23:22:17 +0100

    Create 00022.c

---
+++ text/00022.c
@@ -0,0 +1,10 @@
+// Modification timestamp: 2023-08-03 16:16:30
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00022.c
+
+typedef int x;
+
+int main() {
+    x v;
+    v = 0;
+    return v;
+}
