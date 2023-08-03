7aded97a63f45c7382c3453e2b9386b1a09f1bac
Karine EM <karineven79@gmail.com>
2023-08-03 19:47:44 +0100

    Create 00006.c

---
+++ afl/00006.c
@@ -0,0 +1,17 @@
+// Modification timestamp: 2023-08-02 17:30:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00006.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <value>\n", argv[0]);
+    return 1;
+  }
+
+  int x = atoi(argv[1]);
+  while (x)
+    x = x - 1;
+  return x;
+}
