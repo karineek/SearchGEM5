049467836ddee8894702cc160599179eee79e604
Karine EM <karineven79@gmail.com>
2023-08-03 21:45:18 +0100

    Update 00009.c

--- text/00009.c
+++ text/00009.c
@@ -1,7 +1,5 @@
 // Modification timestamp: 2023-08-02 17:40:00
 // Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00009.c
-#include <stdio.h>
-
 int main() {
     int x;
 
