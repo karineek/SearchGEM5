c47b5efe948fee081169f984031e23e6bb6b797f
Karine EM <karineven79@gmail.com>
2023-08-03 23:21:42 +0100

    Create 00023.c

---
+++ text/00023.c
@@ -0,0 +1,9 @@
+// Modification timestamp: 2023-08-03 16:17:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00023.c
+
+int x;
+
+int main() {
+    x = 0;
+    return x;
+}
