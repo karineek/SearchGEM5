048ef52dc69c73d096f41315a36e195d00111ace
Karine EM <karineven79@gmail.com>
2023-08-03 19:39:31 +0100

    Create 00003.c

---
+++ afl/00003.c
@@ -0,0 +1,15 @@
+// Modification timestamp: 2023-08-02 15:30:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00003.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <value>\n", argv[0]);
+    return 1;
+  }
+
+  int x = atoi(argv[1]);
+  return x - 4;
+}
