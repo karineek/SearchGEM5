54e8b21a654b01b8fa82285e24bee304b06a92c1
Karine EM <karineven79@gmail.com>
2023-08-04 00:32:48 +0100

    Create 00029.c

---
+++ afl/00029.c
@@ -0,0 +1,16 @@
+// Modification timestamp: 2023-08-03 17:17:00
+// Original Source: https://github.com/c-testsuite/c-testsuite/blob/master/tests/single-exec/00029.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <value>\n", argv[0]);
+        return 1;
+    }
+
+    int x;
+    x = atoi(argv[1]);
+    x = x ^ 3;
+    return x - 2;
+}
