a65d3d99a2f42b5f80669ceb8cbf2512a14ad4be
Karine EM <karineven79@gmail.com>
2023-08-08 17:33:44 +0100

    add script to generate corpus of binaries and run tests

---
+++ scripts/test_folder_native.sh
@@ -0,0 +1,19 @@
+folder=$1      # Input folder with C files
+folder_bin=$2  # Output folder to create corpus of binaries
+compiler=$3    # Compiler
+options=$4     # Compilation flags
+logger=$5      # Logger for differential testing
+
+touch $logger
+# Build and produe reference log
+for file in $folder/*.c; do
+    if [[ -f "$file" ]]; then
+        filename=$(basename "$file")
+        $compiler $options $file -o $folder_bin/$filename.o
+        echo ">> $filename.o" >> $logger
+        ./$folder_bin/$filename.o >> $logger 2>&1
+        echo $? >> $logger
+    fi
+done
+echo ">> DONE."
+
