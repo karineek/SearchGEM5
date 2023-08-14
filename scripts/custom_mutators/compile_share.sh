8e9289464e20500a47f4759d88141e260e8b28bf
Karine <karineven79@gmail.com>
2023-08-14 19:04:32 +0100

    add scripts to compiler custom mutator

---
+++ scripts/custom_mutators/compile_share.sh
@@ -0,0 +1,1 @@
+gcc -shared -Wall -O3 -I/home/ubuntu/AFLplusplus/include/ cm-gem5.c -o cm-gem5.so -fPIC
---
+++ scripts/custom_mutators/compile_test.sh
@@ -0,0 +1,1 @@
+ gcc -Wall -O3 -I/home/ubuntu/AFLplusplus/include/ cm-gem5.c -o main_cm-gem5.o -D DEBUG_CM
