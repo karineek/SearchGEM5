c5cab5f4811050ad966a0db7ea7617e3b8ce993c
Karine <karineven79@gmail.com>
2023-08-15 23:18:10 +0100

    update script

--- src/custom_mutators/compile_share.sh
+++ src/custom_mutators/compile_share.sh
@@ -1,1 +1,4 @@
-gcc -shared -Wall -O3 -I/home/ubuntu/AFLplusplus/include/ cm-gem5.c -o cm-gem5.so -fPIC
+gcc -c -fpic -Wall -O3 -I/home/ubuntu/AFLplusplus/include/ /home/ubuntu/AFLplusplus/src/afl-performance.c -o afl-performance.o
+gcc -c -fpic -Wall -O3 -I/home/ubuntu/AFLplusplus/include/ cm-gem5.c -o tmpcm-gem5.o
+#gcc -shared -Wall -O3 -I/home/ubuntu/AFLplusplus/include/ cm-gem5.c -o cm-gem5.so -fPIC
+gcc -shared -o cm-gem5.so afl-performance.o tmpcm-gem5.o
