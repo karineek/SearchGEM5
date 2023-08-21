81b1e9aa2aed428b8b5b5edbb9577177b24a6c3c
Karine <karineven79@gmail.com>
2023-08-21 20:37:01 +0100

    fixing file

--- LLM-corpus-generation/corpus/source/2009-04-16-BitfieldInitialization.c
+++ LLM-corpus-generation/corpus/source/2009-04-16-BitfieldInitialization.c
@@ -2,6 +2,7 @@
 // Original Source: https://github.com/llvm/llvm-test-suite/blob/156ba07a5c779f6b838dac832a25cf7691898288/SingleSource/UnitTests/2009-04-16-BitfieldInitialization.c
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct ty {
   int info;
