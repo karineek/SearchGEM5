d93f7bbc5886a2b369114add66dd093e6e760ab5
Karine EM <karineven79@gmail.com>
2024-02-09 16:05:28 +0000

    Update run-afl.sh

--- scripts/run-afl.sh
+++ scripts/run-afl.sh
@@ -1,20 +1,20 @@
 intput=$1
 output=$2
-logger=$3
-mutators=$4
-script=$5
-gem5_opt=$6
+mutators=$3
+script=$4
+gem5_opt=$5
 afl=$6
-# nohup ./run-afl.sh gpt3.5-old/input/ output/ afl_log.txt "/home/ubuntu/SearchGEM5/src/custom_mutators/cm-gem5.so" 
+# nohup ./run-afl.sh gpt3.5-old/input/ output/ "/home/ubuntu/SearchGEM5/src/custom_mutators/cm-gem5.so" 
 #   "/home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py"
-#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz
+#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz >> $logger 2>&1 &
 #
-# nohup ./run-afl.sh gpt3.5-old/input/ output/ afl_log.txt 
+# nohup ./run-afl.sh gpt3.5-old/input/ output/ 
 #   "/home/ubuntu/SearchGEM5/src/custom_mutators/cm-gem5.so;/home/ubuntu/Gem5Testing/src/custom_mutators/cm-gem5-bin.so;/home/ubuntu/Gem5Testing/src/custom_mutators/cm-gem5-types.so"
 #   "/home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py"
-#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz
+#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz >> $logger 2>&1 &
 
-date > $logger
-AFL_DUMB_FORKSRV=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_CUSTOM_MUTATOR_LIBRARY=$mutators
-$afl -m 50000 -t 99000 -i $intput -o $output -- $gem5_opt $script --isa X86 --input @@ >> $logger 2>&1 &
-date >> $logger
+echo ">> Start fuzzing"
+date
+AFL_DUMB_FORKSRV=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_CUSTOM_MUTATOR_LIBRARY=$mutators $afl -m 50000 -t 99000 -i $intput -o $output -- $gem5_opt $script --isa X86 --input @@
+date
+echo ">> END fuzzing"
