f877bbd54e3ad967964d2e487e58a7b5c99e6250
Karine EM <karineven79@gmail.com>
2024-02-09 16:08:16 +0000

    Update run-afl.sh

--- scripts/run-afl.sh
+++ scripts/run-afl.sh
@@ -5,13 +5,13 @@ script=$4
 gem5_opt=$5
 afl=$6
 # nohup ./run-afl.sh gpt3.5-old/input/ output/ "/home/ubuntu/SearchGEM5/src/custom_mutators/cm-gem5.so" 
-#   "/home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py"
-#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz >> $logger 2>&1 &
+#   /home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py
+#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz >> afl.logger.txt 2>&1 &
 #
 # nohup ./run-afl.sh gpt3.5-old/input/ output/ 
 #   "/home/ubuntu/SearchGEM5/src/custom_mutators/cm-gem5.so;/home/ubuntu/Gem5Testing/src/custom_mutators/cm-gem5-bin.so;/home/ubuntu/Gem5Testing/src/custom_mutators/cm-gem5-types.so"
-#   "/home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py"
-#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz >> $logger 2>&1 &
+#   /home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py
+#   /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/AFLplusplus/afl-fuzz >> afl.logger.txt 2>&1 &
 
 echo ">> Start fuzzing"
 date
