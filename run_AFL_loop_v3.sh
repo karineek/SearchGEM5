ae84347b19e66be3559ccb478f86a95f0845f530
Karine EM <karineven79@gmail.com>
2024-03-10 17:29:25 +0000

    Update run_AFL_loop_v3.sh

--- run_AFL_loop_v3.sh
+++ run_AFL_loop_v3.sh
@@ -1,8 +1,11 @@
+# Single custom mutator as in SSBSE 2023 with saving all test inputs generated. 
+
 # USE FULL PATHS
 cm_path=$1   # E.G. "/home/ubuntu/ASEGem5/src/custom_mutators/"
 afl=$2       # E.G. /home/ubuntu/AFLplusplus/afl-fuzz
 input=$3     # E.G. /home/ubuntu/TinyLlama-test/input
 output=$4    # E.G. /home/ubuntu/TinyLlama-test/output
+resume=$5    # 0 - new run, 1 - resume
 
 AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
 AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5c-save.so" \
