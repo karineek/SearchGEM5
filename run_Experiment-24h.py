a244044e025dd2c7b2f37c578286b20ab1dc7706
Karine EM <karineven79@gmail.com>
2024-03-10 18:58:18 +0000

    Update and rename Experiment-24h.py to run_Experiment-24h.py

---
+++ run_Experiment-24h.py
@@ -0,0 +1,34 @@
+import subprocess
+import sys
+
+def run_bash_script(script_number, param1, param2, param3, param4, param5):
+    script_filename = f"./run_AFL_loop_v{script_number}.sh"
+    try:
+        subprocess.run([script_filename, param1, param2, param3, param4, param5], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running the bash script {script_filename}: {e}")
+
+def main():
+    # Check if the script is called with the correct number of parameters
+    if len(sys.argv) != 7:
+        print("Usage: python script.py <script_number> <param1> <param2> <param3> <param4> <param5>")
+        sys.exit(1)
+
+    # Get the script number and parameters from the command line arguments
+    script_number = int(sys.argv[1])
+    param1 = sys.argv[2]
+    param2 = sys.argv[3]
+    param3 = sys.argv[4]
+    param4 = sys.argv[5]
+    param5 = sys.argv[6]
+
+    # Check if the script number is within the valid range (1 to 8)
+    if 1 <= script_number <= 8:
+        # Run the bash script with additional parameters
+        run_bash_script(script_number, param1, param2, param3, param4, param5)
+        # TODO: stop after 24 hours.
+    else:
+        print("Script number should be between 1 and 8 inclusive.")
+
+if __name__ == "__main__":
+    main()
