9e3b9968d1ed11fdf8654e8593807ea1d8e46aee
Karine EM <karineven79@gmail.com>
2024-03-10 18:56:16 +0000

    Update Experiment-24h.py

--- Experiment-24h.py
+++ Experiment-24h.py
@@ -1,21 +1,28 @@
 import subprocess
-import time
+import sys
 
-def run_bash_script():
+def run_bash_script(script_number):
+    script_filename = f"./run_AFL_loop_v{script_number}.sh"
     try:
-        # Change the path to the actual location of your bash script
-        subprocess.run(["./run_me.sh"], check=True)
+        subprocess.run([script_filename], check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error running the bash script: {e}")
+        print(f"Error running the bash script {script_filename}: {e}")
 
 def main():
-    # Run the bash script initially
-    run_bash_script()
+    # Check if the script is called with the correct number of parameters
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <script_number>")
+        sys.exit(1)
 
-    # Run the bash script every hour for 24 hours
-    for _ in range(24):
-        time.sleep(3600)  # Sleep for 1 hour (3600 seconds)
-        run_bash_script()
+    # Get the script number from the command line argument
+    script_number = int(sys.argv[1])
+
+    # Check if the script number is within the valid range (1 to 8)
+    if 1 <= script_number <= 8:
+        # Run the bash script
+        run_bash_script(script_number)
+    else:
+        print("Script number should be between 1 and 8 inclusive.")
 
 if __name__ == "__main__":
     main()
