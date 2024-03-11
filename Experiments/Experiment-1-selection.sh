99beca1d15fab6c55fd79e864f65bdfda26207bf
Karine <karineven79@gmail.com>
2024-03-11 15:48:16 +0000

    fix  scripts ready for experiment 1

--- Experiments/Experiment-1-selection.sh
+++ Experiments/Experiment-1-selection.sh
@@ -1,18 +1,54 @@
+#!/bin/bash
+# Function to kill script and its child processes
+kill_script() {
+    local parent_pid=$1
+
+    # Get all child processes of the parent PID
+    child_pids=$(pgrep -P $parent_pid)
+
+    # Kill the parent and all its children
+    pkill -TERM -P $parent_pid
+
+    # Wait for processes to be killed
+    sleep 5
+
+    # Forcefully kill any remaining processes
+    pkill -KILL -P $parent_pid
+}
+
 # Assuming run_me.sh is in the current directory
 script="./run_AFL_loop_v" #run_AFL_loop_v1.sh
 
 afl=/home/ubuntu/AFLplusplus/afl-fuzz
+main=/home/ubuntu/TinyLlama-test/
 input=/home/ubuntu/TinyLlama-test/input
 output=/home/ubuntu/TinyLlama-test/output
 
+
+# Total 7200 hours
+
 # Run a configureation script in a loop 30 times
-for ((k = 1; k <= 30; k++)); do
-    script_cong_k=$script$k.sh
-    cm_folders=CM_EXP_$k
-    # Run the script in a loop 30 times
-    for ((i = 1; i <= 30; i++)); do
-        # Redirect the output to repeat_$i.txt and kill after 60 minutes
-        timeout 1s $script_cong_k $cm_folders $afl $input $output$k 0 > "repeat_$i.txt"
+for ((k = 1; k <= 8; k++)); do
+    script_cong_k=$script$k.sh # 1-8 options
+
+    # For each settings out of the 30.
+    for ((j = 1; j <= 30; j++)); do
+        cm_folders_j=CM_EXP_$j
+
+        # Run the script in a loop 30 times
+        for ((i = 1; i <= 30; i++)); do
+            # Redirect the output to repeat_$i.txt and kill after 60 minutes
+            $script_cong_k $cm_folders_j $afl $input $output"_"$k"_setting_"$j"_repeat_"$i 0 > "repeat_script_"$k"_setting_"$j"_repeat_"$i".txt" &
+
+	    # Capture the process ID of the background process
+    	    script_pid=$!
+
+    	    # Sleep for 60 minutes
+    	    sleep 1 #600
+
+    	    # Kill the script after 60 minutes
+     	    kill_script $script_pid
+        done
     done
 done
 
--- Experiments/run_AFL_loop_v1.sh
+++ Experiments/run_AFL_loop_v1.sh
@@ -7,6 +7,7 @@ input=$3     # E.G. /home/ubuntu/TinyLlama-test/input
 output=$4    # E.G. /home/ubuntu/TinyLlama-test/output
 resume=$5    # 0 - new run, 1 - resume
 
+
 # Check if an argument is provided
 if [ $# -le 4 ]; then
     echo "Please provide custom mutator path, afl path, input and output folders, and a value (0 or 1) as a command-line argument."
@@ -19,7 +20,6 @@ else
     resume=""
 fi
 
-$resume AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
-AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5.so" \
-$afl -m 50000 -t 99000 -i $input -o $output -- /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt \
-/home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py --isa X86 --input @@ > $output/all.afl.log 2>&1
+AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
+AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5.so" /home/ubuntu/AFLplusplus/afl-fuzz -m 50000 -t 99000 -i $input -o $output \
+-- /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt /home/ubuntu/gem5-ssbse-challenge-2023/ssbse-challenge-examples/hello-custom-binary-Ex.py --isa X86 --input @@
