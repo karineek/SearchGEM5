f67eb5e03a71871d9068260772e77d892f352c3b
Karine EM <karineven79@gmail.com>
2024-03-11 20:38:42 +0000

    Update run_AFL_loop_v2.sh

--- Experiments/run_AFL_loop_v2.sh
+++ Experiments/run_AFL_loop_v2.sh
@@ -22,4 +22,4 @@ fi
 $resume AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
 AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5c.so" \
 $afl -m 50000 -t 99000 -i $input -o $output -- /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt \
-/home/ubuntu/ASEGem5/hello-custom-binary-Ex.py --isa X86 --input @@ > $input/../all.afl.log 2>&1
+/home/ubuntu/ASEGem5/hello-custom-binary-Ex.py --isa X86 --input @@
