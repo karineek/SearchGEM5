7e3ba0f07212469942bf13eafebec3b3d79870fe
Karine <karineven79@gmail.com>
2024-03-13 10:29:16 +0000

    updating experiment 1

--- Experiments/Experiment-1-selection-full.sh
+++ Experiments/Experiment-1-selection-full.sh
@@ -20,30 +20,30 @@ kill_script() {
 # Run a single experiment
 run_experiment_k() {
     k=$1 # Which version to run
-    j=$2 # Which CM_EXP_ settings to run
 
     script_cong_k=$script$k.sh # 1,5 options
 
     # For each setting out of the 30.
-    # Seq. runs from: for ((j = 1; j <= 30; j++)); do
-    cm_folders_j=CM_EXP_$j
+    for ((j = 1; j <= 30; j++)); do
+        cm_folders_j=CM_EXP_$j
 
-    # Run the script in a loop 30 times
-    for ((i = 1; i <= 30; i++)); do
+        # Run the script in a loop 30 times
+        for ((i = 1; i <= 30; i++)); do
 
-        # Redirect the output to repeat_$i.txt and kill after 60 minutes
-	output_folder=$output"_"$k"_setting_"$j"_repeat_"$i
-        logger="repeat_script_"$k"_setting_"$j"_repeat_"$i".txt"
-        $script_cong_k $cm_folders_j $afl $input $output_folder 0 > $logger &
+            # Redirect the output to repeat_$i.txt and kill after 60 minutes
+	    output_folder=$output"_"$k"_setting_"$j"_repeat_"$i
+            logger="repeat_script_"$k"_setting_"$j"_repeat_"$i".txt"
+            $script_cong_k $cm_folders_j $afl $input $output_folder 0 > $logger &
 
-	# Capture the process ID of the background process
-    	script_pid=$!
+	    # Capture the process ID of the background process
+    	    script_pid=$!
 
-    	# Sleep for 60 minutes
-    	sleep 3600
+    	    # Sleep for 60 minutes
+    	    sleep 3600
 
-    	# Kill the script after 60 minutes
-   	kill_script $script_pid
+    	    # Kill the script after 60 minutes
+     	    kill_script $script_pid
+        done
     done
 }
 
@@ -55,8 +55,7 @@ afl=/home/ubuntu/AFLplusplus/afl-fuzz
 main=$1 # Where is the fuzzing data folder?
 input=$main/input
 output=$main/output
-script_k=$2  # 2 or 6
-CM_ver_j=$3  # 1 to 30
+
 
 # Total 1800 hours
 # 	900 for script 2
@@ -64,6 +63,7 @@ CM_ver_j=$3  # 1 to 30
 
 
 # Run a configuration script in a loop 30 times
-run_experiment_k $script_k $CM_ver_j # No need to save but it requires counters
+run_experiment_k 2 # No need to save but it requires counters
+run_experiment_k 6 # No need to save but it requires counters
 
 echo "Script execution completed."
