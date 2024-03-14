c3f8ac9f8349c340244865339ccfb41d295fca6f
Karine EM <karineven79@gmail.com>
2024-03-14 08:58:07 +0000

    Update run_AFL_loop_v2.sh

--- Experiments/run_AFL_loop_v2.sh
+++ Experiments/run_AFL_loop_v2.sh
@@ -14,13 +14,15 @@ if [ $# -le 4 ]; then
 fi
 
 if [ "$resume" -eq 1 ]; then
+    # Resume fuzzing
     AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_AUTORESUME=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
-AFL_MAP_SIZE=1200000 AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5c.so" \
+AFL_SKIP_BIN_CHECK=1 AFL_MAP_SIZE=1200000 AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5c.so" \
 $afl -m 50000 -t 99000 -i $input -o $output -- /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt \
 /home/ubuntu/ASEGem5/hello-custom-binary-Ex.py --isa X86 --input @@
 else 
+    # Starts a new fuzzing
     AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
-AFL_MAP_SIZE=1200000 AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5c.so" \
+AFL_SKIP_BIN_CHECK=1 AFL_MAP_SIZE=1200000 AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5c.so" \
 $afl -m 50000 -t 99000 -i $input -o $output -- /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt \
 /home/ubuntu/ASEGem5/hello-custom-binary-Ex.py --isa X86 --input @@
 fi
