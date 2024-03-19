43860c55678295297289a94d8102349753ffe5c9
H.D.Menendez Benito <hdmb1r24@srv04141.soton.ac.uk>
2024-03-19 22:00:22 +0000

    fixing dependencies

--- Experiments/Experiment-1-selection.sh
+++ Experiments/Experiment-1-selection.sh
@@ -23,18 +23,20 @@ run_experiment_k() {
     j=$2 # Which CM_EXP_ settings to run
 
     script_cong_k=$script$k.sh # 1,5 options
-
+    echo $script_cong_k
     # For each setting out of the 30.
     # Seq. runs from: for ((j = 1; j <= 30; j++)); do
-    cm_folders_j=$ASE/CM_EXP_$j
-
+    cm_folders_j=${ASE}CM_EXP_$j
+    echo $cm_folders_j
     # Run the script in a loop 30 times
     for ((i = 1; i <= 30; i++)); do
 
         # Redirect the output to repeat_$i.txt and kill after 60 minutes
 	output_folder=$output"_"$k"_setting_"$j"_repeat_"$i
+	echo "output_folder=$output_folder"
         logger="repeat_script_"$k"_setting_"$j"_repeat_"$i".txt"
-        $script_cong_k $cm_folders_j $afl $input $output_folder 0 > /dev/null 2>&1 &
+        echo "${ASE}$script_cong_k $cm_folders_j $afl $input $output_folder 0 2>&1 &"
+	${ASE}$script_cong_k $cm_folders_j $afl $input $output_folder 0 2>&1 &
 	# Getting out of hands with counters.
 
 	# Capture the process ID of the background process
@@ -50,7 +52,7 @@ run_experiment_k() {
 
 ######################### MAIN #########################
 # Assuming run_me.sh is in the current directory
-script="./run_AFL_loop_v" #run_AFL_loop_v1.sh
+script="run_AFL_loop_v" #run_AFL_loop_v1.sh
 
 afl=/home/ubuntu/AFLplusplus/afl-fuzz
 ASE=$1
