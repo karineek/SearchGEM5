30ab91339a7052f22dedd45af3e27d0bf6dda52b
Karine EM <karineven79@gmail.com>
2024-06-04 20:51:43 +0100

    Update get_fuzzing_throughput.sh

--- scripts/throughput/get_fuzzing_throughput.sh
+++ scripts/throughput/get_fuzzing_throughput.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-base=$1
 
 # Loop over all subfolders at the specified depth
-cd $base
 current=`pwd`
-for folder in $base/*/*/*/*/*/input_*; do
+for folder in */*/*/*/*/input_*; do
     # List files with detailed information
     cd "$folder"
     rate=`ls -l id* | wc -l`
+    fuzzproxy=`ls -l fuzz* | wc -l`
     location=`pwd`
-    echo "$location | $rate"
+    echo "$folder,$rate,$fuzzproxy" | sed 's:/:,:g'
     cd $current
 done
