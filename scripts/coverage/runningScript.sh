78d337455e82a1a1a57debc7a1811945ca5f7edf
Karine <karineven79@gmail.com>
2024-06-06 21:29:58 +0100

    fixing the scripts

--- scripts/coverage/runningScript.sh
+++ scripts/coverage/runningScript.sh
@@ -7,12 +7,27 @@ model=$2 # Phi, CodeBooga, Llama, Magicoder, TinyLlama, gpt3.5-new, and gpt3.5-o
 # Fix parameters:
 home=/home/debian
 gfauto=$home/graphicsfuzz/gfauto/
+data=$home/experiment/$model/
 
+# docker run -it -v /home/ubuntu/phi-cmin/part-1/output-2-Phi-cmin-1/10/Phi-cmin/:/mnt/shared eab7d0167a93
 if [ "$op" = "init" ]; then
-	exp=$home/experiment/$model/input/
+	exp=$data/input/
+        mkdir -p $data
+        cp -r /mnt/shared/input $data
+        cp -r /mnt/shared/binary $data
 	./1-wrapper-get-coverage.sh $home $exp 0 5 $gfauto func-gem5-llm-$model.csv lines-gem5-llm-$model.csv report-gem5-llm-$model.csv
+        tar -czvf $model-gcov-0.tar.gz $home/coverage_gcda_files $home/coverage_processed >> llm_coverage_output_tar.txt 2>&1
+        cp  $model-gcov-0.tar.gz /mnt/shared/
 elif [ "$1" = "cont" ]; then
-	exp=$home/experiment/output-uniq/
+	exp=$home/experiment/$model/input_fuzzed
+        mkdir -p $data
+        cp -r /mnt/shared/input_fuzzed/ $data
+        cp -r /mnt/shared/binary $data
+        cp /mnt/shared/$model-gcov-0.tar.gz $home
+        current=`pwd`
+        cd $home
+        tar -xf $model-gcov-0.tar.gz
+        cd $current
 	./1-wrapper-get-coverage-cont.sh $home $exp 1 5 $gfauto func-gem5-afl-$model.csv lines-gem5-afl-$model.csv report-gem5-afl-$model.csv
 elif [ "$op" = "sliced" ]; then
 	exp=$home/experiment/$model/split/
