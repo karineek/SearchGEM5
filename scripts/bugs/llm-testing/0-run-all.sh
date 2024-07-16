3e9e4a9a7b00cdfbd0ddd304b659b28047e2a375
Karine EM <karineven79@gmail.com>
2024-07-16 10:12:58 +0100

    Update 0-run-all.sh

--- scripts/bugs/llm-testing/0-run-all.sh
+++ scripts/bugs/llm-testing/0-run-all.sh
@@ -1,7 +1,10 @@
-#./1-run-diff-test.sh /home/ubuntu/experiment-7/CodeBooga/input
-#./1-run-diff-test.sh /home/ubuntu/experiment-7/Llama/input
-./1-run-diff-test.sh /home/ubuntu/experiment-7/Magicoder/input
-./1-run-diff-test.sh /home/ubuntu/experiment-7/Phi/input
-./1-run-diff-test.sh /home/ubuntu/experiment-7/TinyLlama/input
-./1-run-diff-test.sh /home/ubuntu/experiment-7/gpt3.5-new/input
-./1-run-diff-test.sh /home/ubuntu/experiment-7/gpt3.5-old/input
+inputs=$1 # e.g. /home/ubuntu/experiment-7/
+
+./1-run-diff-test.sh $1/CodeBooga/input
+./1-run-diff-test.sh $1/Llama/input
+./1-run-diff-test.sh $1/Magicoder/input
+./1-run-diff-test.sh $1/Phi/input
+./1-run-diff-test.sh $1/TinyLlama/input
+./1-run-diff-test.sh $1/gpt3.5-new/input
+./1-run-diff-test.sh $1/gpt3.5-old/input
+echo ">> Done."
